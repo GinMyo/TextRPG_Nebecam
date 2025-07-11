338e686ddfadee435eea731f766201c8ffc15280
GinMyo <rlsayay101@gmail.com>
2025-07-11 09:16:57 +0900

    캐릭터 생성 메서드 수정
    1. 입력 값을 PlayerStatus 필드로 대입되게 변경

--- TextRPG/TextRPG/GameBoard.cs
+++ TextRPG/TextRPG/GameBoard.cs
@@ -1,14 +1,5 @@
 ﻿namespace TextRPG
 {
-    public enum Scenes      //게임 씬 종류
-    {
-        MainScene = 1,
-        StatusScene,
-        InventoryScene,
-        ShopScene
-    }
-
-
     public class PlayerStatus       // 캐릭터 스텟
     {
         public string name = "";
@@ -31,21 +22,21 @@
             if (isMakeCharacter == false)
             {
                 MakeCharacter();
+                isMakeCharacter = true;
+                Console.Clear();
             }
             
-            Console.WriteLine();
+            
         }
 
         static void MakeCharacter()
         {
             PlayerStatus playerStatus = new PlayerStatus();
-            string name = playerStatus.name;
-            string job = playerStatus.job;
 
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.Write("원하시는 이름을 설정해주세요. \n\n>> ");
-            name = Console.ReadLine();
-            Console.Write($"\n입력하신 이름은 {name} 입니다.");
+            playerStatus.name = Console.ReadLine();
+            Console.Write($"\n입력하신 이름은 {playerStatus.name} 입니다.");
 
 
             while (true)        // 올바른 값 입력할 때 까지 무한 루프
@@ -55,23 +46,22 @@
                 Console.WriteLine("2. 도적");
                 Console.Write("\n\n>> ");
 
-                job = Console.ReadLine();
+                int jobInput = int.Parse(Console.ReadLine());
 
-                switch (job)
+                switch (jobInput)
                 {
-                    case "1":
-                        Console.WriteLine("전사를 선택하셨습니다. ");
+                    case 1:
+                        playerStatus.job = "전사";
                         break;
 
-                    case "2":
-                        Console.WriteLine("도적을 선택하셨습니다. ");
+                    case 2:
+                        playerStatus.job = "도적";
                         break;
 
                     default:
-                        Console.WriteLine("올바른 값을 입력해주세요. ");
+                        Console.WriteLine("올바른 값을 입력해주세요.");
                         continue;
                 }
-
                 break;
             }
 
